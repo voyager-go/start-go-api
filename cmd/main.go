9f282ebf1e1038a76a23e6bdceab30f38e0ede16
youeryuango <893774306@qq.com>
2021-12-15 19:21:54 +0800

    启动服务测试

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/voyager-go/start-go-api/bootstrap"
 	_ "github.com/voyager-go/start-go-api/bootstrap"
 )
 
 func main() {
+	bootstrap.RunService()
 	r := gin.Default()
 	r.Run(":8090")
 }
